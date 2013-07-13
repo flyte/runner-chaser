48e7b85c99d5a99f8cf71b324d2df199173ed74a
Flyte <flyte@failcode.co.uk>
2013-07-13 14:48:05 +0100

    Disable chaser for now. Added visualisation of A* algorithm

--- python/runner-chaser.py
+++ python/runner-chaser.py
@@ -3,6 +3,7 @@ from abc import ABCMeta, abstractmethod
 from time import sleep
 from random import randint, choice
 from math import ceil
+from numpy import interp
 
 def enum(*sequential, **named):
     """
@@ -280,7 +281,7 @@ class Game(object):
 
     def __init__(self, grid_size, runner_start_pos, chaser_start_pos):
         self.grid = Grid(grid_size)
-        self.runner = Runner(runner_start_pos, (0, 0, 255), 2)
+        self.runner = Runner(runner_start_pos, (0, 0, 255), 1)
         self.chaser = Chaser(chaser_start_pos, (255, 0, 0))
         self.apples = []
         self.refill_apples()
@@ -338,17 +339,18 @@ class Player(object):
         ## The further the better for the runner, the closer the better for the chaser.
         viable_apples = []
         for apple in game.apples:
-            distance = Grid.distance(self.character.position, apple.position, self.character.max_moves_per_turn)
+            distance = Grid.distance(self.character.position, apple.position,
+                self.character.max_moves_per_turn)
             if distance <= apple.shelf_life:
                 viable_apples.append({ "apple": apple, "distance": distance })
         
         return sorted(viable_apples, key=lambda a: a["distance"])
         
     def make_move(self):
-        try:
-            target_coords = self.find_path()[1].position
-        except:
-            target_coords = self.find_target_coords()
+        path = self.find_path()
+        target_coords = path[1].pos if len(path) > 1 else path[0].pos
+
+        #target_coords = self.find_target_coords()
         
         if not target_coords:
             self.character.move_noop(self.game.grid)
@@ -388,8 +390,8 @@ class ChaserPlayer(Player):
 
 class AStarNode(object):
 
-    def __init__(self, position, g, h):
-        self.position = position
+    def __init__(self, pos, g, h):
+        self.pos = pos
         self.f = g + h
         self.g = g
         self.h = h
@@ -400,6 +402,7 @@ class RunnerPlayer(Player):
     def __init__(self, game):
         super(RunnerPlayer, self).__init__(game)
         self.character = game.runner
+        self.window = window
 
     def heuristic_distance(self, target_coords):
         """
@@ -419,58 +422,103 @@ class RunnerPlayer(Player):
             # If pos in the danger zone, add some cost
             chaser_distance = Grid.distance(pos, self.game.chaser.position, 1)
             if chaser_distance <= danger_zone:
-                cost += int(danger_zone - chaser_distance + 1)
+                extra_cost = int(danger_zone - chaser_distance + 1) * 5
+                cost += extra_cost
+                log("Added %d cost for being in the danger zone." % extra_cost)
                 
         return cost
 
     def find_path(self):
+        """
+        Use A* algorithm to plot the path with the least cost from the current position
+        to the target (apple).
+        Returns list of coords starting with current position eg.
+        [
+            (10, 8),
+            (10, 10),
+            (12, 10)
+        ]
+        """
         target_coords = self.find_target_coords(avoid_chaser=False)
         if not target_coords:
-            return [self.character.position]
+            return [AStarNode(self.character.position, 0, 0)]
         
+        # Add the current position to open_set
         start_node = AStarNode(self.character.position, 0, self.heuristic_distance(target_coords))
-        open_set = { start_node.position: start_node }
+        open_set = { start_node.pos: start_node }
         closed_set = {}
         came_from = {}
         
         while len(open_set):
-            node_current = sorted(open_set.iteritems(), key=lambda n: n[1].f)[0][1]
-            if node_current.position == target_coords:
-                return self.reconstruct_path(came_from, node_current)
+            # Get the node from open_set which has the least amount of total cost (f)
+            nc = sorted(open_set.iteritems(), key=lambda n: n[1].f)[0][1]
             
-            del open_set[node_current.position]
-            closed_set[node_current.position] = node_current
+            # If we've reached our goal, reconstruct the path and return it
+            if nc.pos == target_coords:
+                path = self.reconstruct_path(came_from, nc)
+                # Fill in path
+                for node in path:
+                    pygame.draw.circle(window, (0, 0, 255), get_position(node.pos), 10, 2)
+                    pygame.display.flip()
+                sleep(1)
+                return path
             
+            # Remove the current node from open_set and add it to closed_set
+            del open_set[nc.pos]
+            closed_set[nc.pos] = nc
+            
+            # Get all surrounding coordinates within our character's max_moves_per_turn radius.
+            # Create nodes for each of the coordinates including distance from current node and
+            # heuristic distance (cost) from current character position.
             node_successors = [
                 AStarNode(pos,
-                    Grid.distance(node_current.position, pos, self.character.max_moves_per_turn) + \
-                        node_current.g,
+                    Grid.distance(nc.pos, pos, self.character.max_moves_per_turn) + nc.g,
                     self.heuristic_distance(target_coords)
-                ) for pos in self.game.grid.surrounding_valid_coords(node_current.position,
+                ) for pos in self.game.grid.surrounding_valid_coords(nc.pos,
                     self.character.max_moves_per_turn)
             ]
             
-            for node_successor in node_successors:
-                if node_successor.position in closed_set and (
-                    node_successor.g >= closed_set[node_successor.position].g):
+            # For each of the nodes surrounding the current node
+            for ns in node_successors:
+                # If we've evaluated this neighbor node before and it took the same or more
+                # cost to get to it this time then move on.
+                if ns.pos in closed_set and ns.g >= closed_set[ns.pos].g:
                     continue
                 
-                if node_successor.position not in open_set or (
-                    node_successor.position in closed_set and \
-                    node_successor.g < closed_set[node_successor.position].g):
-                    came_from[node_successor.position] = node_current
-                    if node_successor.position not in open_set:
-                        open_set[node_successor.position] = node_successor
+                if ns.pos in open_set and ns.g >= open_set[ns.pos].g:
+                    continue
+                
+                # If we haven't listed this node for evaluation or if we have, but our current
+                # route to the node is more efficient
+                #if ns.pos not in open_set or (ns.pos in closed_set and ns.g < closed_set[ns.pos].g):
+ 
+                if ns.pos in closed_set: del closed_set[ns.pos]
+                if ns.pos in open_set: del open_set[ns.pos] 
+ 
+                # Add the current node as the originating node for this coordinate
+                came_from[ns.pos] = nc
+                
+                # If this neighbor has not been listed for evaluation, list it
+                if ns.pos not in open_set:
+                    open_set[ns.pos] = ns
+            
+            # Fill in closed set
+            for coords, node in closed_set.iteritems():
+                red = interp(node.f, (0, 25), (0, 255))
+                pygame.draw.circle(window, (red, 255 - red, 0), get_position(coords), 5)
+                pygame.display.flip()
         
-        return None
+        # We didn't reach our goal, so return our current position only
+        return [start_node]
         
-    def reconstruct_path(self, came_from, current_node):
-        if current_node.position in came_from:
-            path = self.reconstruct_path(came_from, came_from[current_node.position])
-            path.append(current_node)
-            return path
+    def reconstruct_path(self, came_from, nc):
+        """
+        Recursive function to build a list of coordinates from target, back to character.position
+        """
+        if nc.pos in came_from:
+            return self.reconstruct_path(came_from, came_from[nc.pos]) + [nc]
         else:
-            return [current_node]
+            return [nc]
         
 
     def find_target_coords(self, avoid_chaser=True):
@@ -583,7 +631,8 @@ def draw_all(game, window):
 if __name__ == "__main__":
     grid_size = (16, 9)
     runner_start_pos = (0, grid_size[1] - 1)
-    chaser_start_pos = (grid_size[0] - 1, 0)
+#    chaser_start_pos = (grid_size[0] - 1, 0)
+    chaser_start_pos = (grid_size[0] / 2, grid_size[1] / 2)
     
     game = Game(grid_size, runner_start_pos, chaser_start_pos)
     
@@ -601,6 +650,7 @@ if __name__ == "__main__":
     while True:
         for c in [ p_runner, p_chaser ]:
             c.make_move()
+            break
 
         try:
             game.tick()
@@ -615,7 +665,7 @@ if __name__ == "__main__":
         if game.runner.score != previous_scores[0] or game.chaser.score != previous_scores[1]:
             print "Runner score: %d, Chaser score: %d" % (game.runner.score, game.chaser.score)
             previous_scores = [ game.runner.score, game.chaser.score ]
-        sleep(0.1)
+        sleep(0.25)
     
     draw_all(game, window)
     
